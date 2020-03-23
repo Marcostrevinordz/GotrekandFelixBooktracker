71f51edbfe054baa3f872aaf34433fc64d669304
Marcos Treviño Rodriguez <marcostrevinordz@gmail.com>
2020-03-23 23:10:38 +0100

    New Repository

---
+++ script.js
@@ -0,0 +1,12 @@
+	var get_date = "Type the date";
+
+
+function myfunction() {
+	var get_words = prompt("Completed on?");
+	var get_date = get_words;
+	if(get_date == null) {
+		get_date = "Pending";
+	}
+	document.getElementById("Trollslayer").innerHTML = "Completed on: " + get_date;
+	document.getElementById("Trollslayer").className = "done";
+}
